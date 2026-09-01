8db52be5c8e4b08c9c59bd47b8e2d5f816926eb8
AnushaSrivastava007 <Anusha.34685@stu.upes.ac.in>
2026-09-01 18:24:45 +0530

    Implement basic arithmetic operations in program3.c

---
+++ program3.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+  float a,b;
+printf("Enter the 2 numbers");
+scanf("%f %f",&a,&b);
+printf("Sum=%.2f\n",a+b);
+printf("Difference=%.2f\n",a-b);
+printf("Multiplication=%.2f\n",a*b);
+printf("Division=%.2f\n",a/b);
+return 0;
+}
