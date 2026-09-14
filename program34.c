958f556d45807fd321cbd6ea30d8ce7c519b29a1
AnushaSrivastava007 <Anusha.34685@stu.upes.ac.in>
2026-09-15 00:42:02 +0530

    Implement prime number

---
+++ program34.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+ int n, i, flag = 0;
+ printf("Enter the number:");
+scanf("%d", &n);
+if(n <= 1)
+{
+  printf("Not prime");
+ }
+  else
+ {
+   for(i = 2; i < n; i++)
+  {
+  if(n % i == 0)
+  {
+  flag = 1;
+  break;
+  }}
+ if(flag == 0)
+ {
+   printf("Prime");
+ }
+ else
+ {
+  printf("Not prime");
+ }}
+    return 0;
+}
