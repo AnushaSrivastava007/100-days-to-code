890acd7b7b6d8d4a8310f5c99ffb19b63f2dcf2f
AnushaSrivastava007 <Anusha.34685@stu.upes.ac.in>
2026-09-15 00:25:25 +0530

    Add program to calculate product of even numbers

---
+++ program28.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n, i, product = 1;
+printf("Enter the number:"); 
+scanf("%d", &n);
+for(i = 2; i <= n; i = i + 2)
+ {
+ product = product * i;
+ }
+ printf("%d", product);
+return 0;
+}
