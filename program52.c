2b6dbb4e734e258e15aaedf90bc03fc3636bac67
AnushaSrivastava007 <Anusha.34685@stu.upes.ac.in>
2026-09-19 15:05:07 +0530

    pattern

---
+++ program52.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        int star;
+        if (i <= 3)
+        star = 2 * i - 1;
+        else
+        star = 2 * (6 - i) - 1;
+        for (j = 1; j <= star; j++)
+        {
+        printf("*\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
